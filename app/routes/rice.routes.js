9d4a9cdede58001256ed5ac6b053b5251219ea05
somchit chanudom <somchit.c@nexts-corp.com>
2017-07-03 17:52:21 +0700

    update report  4

--- app/routes/rice.routes.js
+++ app/routes/rice.routes.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.route('/report1').get(rice.report1);
     app.route('/report2').get(rice.report2);
     app.route('/report3').get(rice.report3);
-    app.route('/report3_1/:id').get(rice.report3_1);
+    app.route('/report3_1').get(rice.report3_1);
     app.route('/report3_2/:id').get(rice.report3_2);
     app.route('/report4').get(rice.report4);
 }
