bf0329a9237a13b8e9b491ffa72cf8f9f467acbd
somchit chanudom <somchit.c@nexts-corp.com>
2017-07-04 11:22:04 +0700

    report4 แก้

--- app/routes/finance.routes.js
+++ app/routes/finance.routes.js
@@ -1,13 +1,13 @@
 module.exports = function (app) {
-    var payment = require('../controllers/payment.controller');
-    app.route('/report1').get(payment.report1);
-    app.route('/report2').get(payment.report2);
-    // app.route('/report3_1/:id').get(payment.report3_1);
-    // app.route('/report3_2/:id').get(payment.report3_2);
-    app.route('/report4/:id').get(payment.report4);
-    app.route('/report6/:id').get(payment.report6);
-    app.route('/report7').get(payment.report7); //ok
-    app.route('/report8').get(payment.report8); //ok
-    app.route('/report10/:id').get(payment.report10); //ok
-    app.route('/report11').get(payment.report11);
+    var finance = require('../controllers/finance.controller');
+    app.route('/report1').get(finance.report1);
+    app.route('/report2').get(finance.report2);
+    // app.route('/report3_1/:id').get(finance.report3_1);
+    // app.route('/report3_2/:id').get(finance.report3_2);
+    app.route('/report4/:id').get(finance.report4);
+    app.route('/report6/:id').get(finance.report6);
+    app.route('/report7').get(finance.report7); //ok
+    app.route('/report8').get(finance.report8); //ok
+    app.route('/report10/:id').get(finance.report10); //ok
+    app.route('/report11').get(finance.report11);
 }
