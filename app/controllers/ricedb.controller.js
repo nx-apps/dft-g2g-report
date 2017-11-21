cd5bf09d20980a5578bdaa6806f51661c5b0f5c5
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-11-21 17:42:35 +0700

    ricedb follow report

--- app/controllers/ricedb.controller.js
+++ app/controllers/ricedb.controller.js
@@ -3,5 +3,17 @@ exports.follow1 = (req, res) => {
         result = JSON.parse(result);
         res.ireport("ricedb/report_follow1.jasper", req.query.export || "pdf", result, {});
     });
-
-}
+};
+exports.follow2 = (req, res) => {
+    req.jdbc.query('mssql', `exec sp_rpt_follow2`, [], (err, result) => {
+        result = JSON.parse(result);
+        res.ireport("ricedb/report_follow2.jasper", req.query.export || "pdf", result, {});
+    });
+};
+exports.follow3 = (req, res) => {
+    req.jdbc.query('mssql', `exec sp_rpt_follow3`, [], (err, result) => {
+        result = JSON.parse(result);
+        // res.json(result)
+        res.ireport("ricedb/report_follow3.jasper", req.query.export || "pdf", result, {});
+    });
+};
--- app/routes/ricedb.routes.js
+++ app/routes/ricedb.routes.js
@@ -1,4 +1,6 @@
 module.exports = function (app) {
     var controller = require('../controllers/ricedb.controller');
     app.route('/follow1').get(controller.follow1);
+    app.route('/follow2').get(controller.follow2);
+    app.route('/follow3').get(controller.follow3);
 }
