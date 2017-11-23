dd392c74aaca1f58923da4678b7dbc13680c861c
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-11-23 15:14:46 +0700

    add xlsx

--- app/controllers/ricedb.controller.js
+++ app/controllers/ricedb.controller.js
@@ -11,11 +11,58 @@ exports.follow1 = (req, res) => {
 exports.follow2 = (req, res) => {
     req.jdbc.query('mssql', `exec sp_rpt_follow2`, [], (err, result) => {
         result = JSON.parse(result);
-        const params = {
-            FILE_TYPE: req.query.export || "pdf",
-            OUTPUT_NAME: 'ข้อมูลคลังสินค้าที่ค้างรับมอบข้าว'
-        };
-        res.ireport("ricedb/report_follow2.jasper", params.FILE_TYPE, result, params);
+        if (req.query.export === 'excel2') {
+            // console.log('excel', result);
+            const XLSX = require('xlsx');
+            /* create workbook & set props*/
+            const wb = {
+                SheetNames: [], Sheets: {}
+            };
+            /*create sheet data & add to workbook*/
+            const ws = XLSX.utils.json_to_sheet(result,
+                {
+                    header: [
+                        "follow_year",
+                        "follow_no",
+                        "follow_type",
+                        "associate",
+                        "warehouse_name",
+                        "province_name",
+                        "typerice_name",
+                        "project_name",
+                        "bidder_name",
+                        "weight_approve",
+                        "weight_contract",
+                        "weight_received",
+                        "weight_balance",
+                        "weight_lost",
+                        "contract_no",
+                        "save_date",
+                        "limit_date",
+                        "remark",
+                        "problem_found",
+                        "problem_fix",
+                        "contract_mistake"
+                    ]
+                });
+            const ws_name = "Sheet1";
+            XLSX.utils.book_append_sheet(wb, ws, ws_name);
+
+            /* create file 'in memory' */
+            const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'buffer' });
+            const filename = encodeURIComponent('ข้อมูลคลังสินค้าที่ค้างรับมอบข้าว.xlsx');
+            res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+            res.type('application/octet-stream');
+            res.send(wbout);
+            // res.json(result);
+        } else {
+            const params = {
+                FILE_TYPE: req.query.export || "pdf",
+                OUTPUT_NAME: 'ข้อมูลคลังสินค้าที่ค้างรับมอบข้าว'
+            };
+            res.ireport("ricedb/report_follow2.jasper", params.FILE_TYPE, result, params);
+        }
+
     });
 };
 exports.follow3 = (req, res) => {
